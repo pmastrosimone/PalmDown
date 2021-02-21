55e757fd09f0a3694fbfd9f36ba36b18ac1fc1bd
pmastrosimone <66535355+pmastrosimone@users.noreply.github.com>
2021-02-21 14:19:34 -0500

    Init Commit

---
+++ Rsc/PalmDown_Rsc.h
@@ -0,0 +1,7 @@
+/* pilrc generated file.  Do not edit!*/
+#define MainClearTextButton 1008
+#define MainDescriptionField 1007
+#define MainForm 1006
+#define RomIncompatibleAlert 1005
+#define EditOnlyMenuBar 1003
+#define MainMenuBar 1001
